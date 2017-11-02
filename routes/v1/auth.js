bff84a79825ef9d79d01d5e65aac93a2507009ce
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-11-02 15:12:39 +0800

    nodemailer introduced; Send a confirmation email to user once they signed up. User cannot login unless they press the link in the email.

--- routes/v1/auth.js
+++ routes/v1/auth.js
@@ -3,7 +3,17 @@ const { check, validationResult } = require('express-validator/check');
 const { matchedData, sanitize } = require('express-validator/filter');
 var jwt = require('jsonwebtoken'); // sign with default (HMAC SHA256)
 var bcrypt = require('bcrypt');
+var nodemailer = require('nodemailer');
 const saltRounds = 10;
+const confirmationUrl = "http://localhost:8000/v1/auth/confirm";
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.email,
+        pass: process.env.email_password
+    }
+});
 
 router.post(
     '/register',
@@ -86,6 +96,36 @@ router.post(
 
             };
 
+            // send email to this user email for confirmation
+            const sendConfirmationEmail = () => {
+
+                return new Promise((resolve, reject)=>{
+
+                    // sign a confirmation token first
+                    let token = jwt.sign({ data: { 'e': user.email, 'ct': true } }, process.env.jwtSecret, { expiresIn: '3h' });
+
+                    // then send the email to the user with the token parameters
+                    var mailOptions = {
+                        from: process.env.email,
+                        to: user.email,
+                        subject: 'Sending Email using Node.js',
+                        html: `<h1>Welcome</h1><p>Confirm this registration pls 2</p><a href=${confirmationUrl + '?ctoken=' + token} target="_blank">Confirm</a>`
+                    };
+
+                    transporter.sendMail(mailOptions, (error, info) => {
+                        if (error) {
+                            console.log(error);
+                            reject(error);
+                        }
+                        else {
+                            console.log('Email sent: ' + info.response);
+                            resolve(info.response);
+                        }
+                    });
+                });
+
+            }
+
             checkEmailInDB().then((result) => {
 
                 // hash the user password
@@ -95,6 +135,11 @@ router.post(
 
                 // store hash password in DB
                 return registerUser(hash);
+
+            }).then(()=>{
+
+                // send the confirmation email to the user pls
+                return sendConfirmationEmail();
 
             }).then((result)=>{
                 
@@ -142,10 +187,11 @@ router.post(
                 return new Promise((resolve, reject) => {
                     // find the user in the db by using the email
                     db.query(
-                        'SELECT password FROM users WHERE email=?',
+                        'SELECT password, confirm FROM users WHERE email=?',
                         [user.email],
                         (dberror, results, fields) => {
                             if (dberror) {
+                                console.log('db error when searching password from email');
                                 // send db error if got any
                                 return reject(dberror);
                             }
@@ -153,7 +199,16 @@ router.post(
 
                                 if (results.length > 0) {
                                     // results is not empty
-                                    resolve(results[0].password);
+
+                                    if (!results[0].confirm) {
+                                        // if the user havent confirm this email, reject
+                                        reject({ emailconfirm: false });
+                                    }
+                                    else {
+                                        // if the user alr confirm this email, then carry on
+                                        resolve(results[0].password);
+                                    }
+
                                 }
                                 else {
                                     // cannot find this user email in the db
@@ -226,5 +281,61 @@ router.post(
 
     }
 );
+
+router.get('/confirm', (req, res)=>{
+    // get the confirmation token
+    let ctoken = req.query.ctoken;
+
+    if(ctoken) {
+
+        // first check for token validation
+        jwt.verify(ctoken, process.env.jwtSecret, (err, decoded) => {
+
+            if (err) {
+                // json token maybe expired
+                return res.json({ success: false, errors: { jwt: 'json web token validate error, registration not successful' } });
+            }
+            else {
+
+                if (decoded.data.ct) {
+
+                    // then update db about confimation
+                    // connect to mariadb/mysql
+                    const db = require('../../db.js');
+                    db.query(
+                        'UPDATE users SET confirm=1 WHERE email=(?)',
+                        [decoded.data.e],
+                        (dberror, results, fields) => {
+                            if (dberror) {
+                                // send db error if got any
+                                console.log(dberror);
+                                res.send('Invalid Token, u hacka?');
+                            }
+                            else {
+
+                                // successfully update the user confirmation
+                                res.send('Thank you for joining! ' + decoded.data.e);
+
+                            }
+                        }
+                    );
+
+                }
+                else {
+                    res.send('Invalid Token, u hacka?');
+                }
+
+            }
+
+        });
+
+    }
+    else {
+        res.send('Invalid Token, u hacka?');
+    }
+
+});
+
+
 
 module.exports = router;
