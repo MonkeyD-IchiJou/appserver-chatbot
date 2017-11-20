cdb8bd7fc9a0084479c6d869b17bbbff9207f460
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-11-20 18:24:43 +0800

    will load the .env file if havent loaded beforehand

--- loadenv.js
+++ loadenv.js
@@ -1,9 +1,13 @@
 var loadenv = () => {
-    // explicitly loading variables from .env files if is in development
-    require('dotenv').load();
 
-    // check which modes the app is running
-    console.log('NODE_ENV: ' + process.env.NODE_ENV + ' mode');
+    if (!process.env.NODE_ENV) {
+        // explicitly loading variables from .env files if is in development
+        require('dotenv').load();
+
+        // check which modes the app is running
+        console.log('NODE_ENV: ' + process.env.NODE_ENV + ' mode');
+    }
+
 }
 
 module.exports = loadenv;
